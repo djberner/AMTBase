69b6bdb2c0c247e9647f9a65075175d946f16f44
David Berner <David.Berner@amtgroupuk.com>
2018-01-17 12:40:00 +0000

    Updated Brand Logos, tested out funders table

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -12,10 +12,10 @@ Yii::$app->language='en-GB';
 
 class ProductController extends Controller
 {
- public function actionCategory($categories)
+    public function actionCategory($categories)
     {
         $params = explode('/', $categories);
-		
+
         if (sizeof($params)==4){
             $product = Yii::$app->db->createCommand('SELECT * FROM tblbase WHERE Friendly_URL_4="'.$categories.'"')->queryOne();
             $specs = Yii::$app->db->createCommand('SELECT Technical_Category_Description,Technical_Long_Description,Technical_Value FROM tblspec WHERE Derivative_CAP_Code="'.$product["Derivative_CAP_Code"].'" ORDER BY Technical_Category_Description ')->queryAll();
@@ -29,19 +29,19 @@ class ProductController extends Controller
         }
         if (sizeof($params)==1){
             $page = Yii::$app->db->createCommand('SELECT * FROM tblpages WHERE PageName="manufacturer_'.strtolower ($params[0]).'"')->queryOne();
-            $vehiclemodels = Yii::$app->db->createCommand('SELECT DISTINCT Range_Name FROM tblbase WHERE Manufacturer_Name="'.$params[0].'" ORDER BY Range_Name ')->queryAll();
+            $vehiclemodels = Yii::$app->db->createCommand('SELECT DISTINCT Range_Name FROM tblbase WHERE Vehicle_Type ="Car" AND Manufacturer_Name="'.$params[0].'" ORDER BY Range_Name ')->queryAll();
             foreach ($vehiclemodels as $arr_veh_model) {
                 $samplevehiclemodels = Yii::$app->db->createCommand('SELECT tblbase.*, tblfunder.Derivative_Monthly_Rental_Price,timage.Image_Set_ID FROM tblbase JOIN tblfunder
-    ON tblbase.Derivative_CAP_Code = tblfunder.Derivative_CAP_Code JOIN timage ON tblbase.Derivative_CAP_ID=timage.CAP_Id WHERE Range_Name="'.$arr_veh_model["Range_Name"].'" ORDER BY tblfunder.Derivative_Monthly_Rental_Price ')->queryOne();
+    ON tblbase.Derivative_CAP_Code = tblfunder.Derivative_CAP_Code JOIN timage ON tblbase.Derivative_CAP_ID=timage.CAP_Id WHERE  tblbase.Vehicle_Type ="Car" AND Manufacturer_Name = "'.$params[0].'" AND Range_Name="'.$arr_veh_model["Range_Name"].'" ORDER BY tblfunder.Derivative_Monthly_Rental_Price ')->queryOne();
                 $samplevehiclemodelscount = Yii::$app->db->createCommand('SELECT count(Range_Name) as NumDealsAvailable  FROM tblbase JOIN tblfunder
-    ON tblbase.Derivative_CAP_Code = tblfunder.Derivative_CAP_Code WHERE Range_Name="'.$arr_veh_model["Range_Name"].'"')->queryOne();
+    ON tblbase.Derivative_CAP_Code = tblfunder.Derivative_CAP_Code WHERE  Vehicle_Type ="Car" AND Range_Name="'.$arr_veh_model["Range_Name"].'"')->queryOne();
                 $samplevehicleurbanmpg = Yii::$app->db->createCommand('SELECT Technical_Short_Description,Technical_Value FROM tblspec  
   WHERE Derivative_CAP_Code="'.$samplevehiclemodels["Derivative_CAP_Code"].'" AND Technical_Short_Description IN ("EC Urban (mpg)","EC Urban")')->queryOne();
                 $samplevehiclempg = Yii::$app->db->createCommand('SELECT Technical_Short_Description,Technical_Value FROM tblspec  
   WHERE Derivative_CAP_Code="'.$samplevehiclemodels["Derivative_CAP_Code"].'" AND Technical_Short_Description IN ("EC Extra Urban (mpg)","EC Extra Urban")')->queryOne();
-        /*        echo "<pre>";
-                var_dump($samplevehiclemodelscount);
-                echo "</pre>";*/
+                /*        echo "<pre>";
+                        var_dump($samplevehiclemodelscount);
+                        echo "</pre>";*/
 
 
 
@@ -72,9 +72,10 @@ class ProductController extends Controller
                 }
 
             }
+            
             return $this->render('manufacturer',array('page'=>$page,'samplevehicle'=>$samplevehicle));
 
         }
 
-     }
+    }
 }
